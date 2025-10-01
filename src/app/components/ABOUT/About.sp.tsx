f49bf56057fc3c45c6faf92ddbff46af285c423e
rikuto06 <zhongweiluren0@gmail.com>
2025-10-01 21:53:43 +0900

    fix

--- src/app/components/ABOUT/About.sp.tsx
+++ src/app/components/ABOUT/About.sp.tsx
@@ -10,15 +10,15 @@ export default function Aboutsp() {
   const [index,setIndex] = React.useState(0);
 
   const handleTap = () => {
-      setIndex((prev)=>(prev + 1) % images.length );
-    
+     const randomIndex = Math.floor(Math.random() * images.length);
+    setIndex(randomIndex);
   };
 
   return (
     <section className='bg-[#001A00] pt-[49px] px-[5.3%]'>
       <h2 className={`${oswald.className} text-[6.4rem] text-transparent text-[6.5rem] font-bold leading-[100%] [-webkit-text-stroke-width:1px] [-webkit-text-stroke-color:#F5F5F5] text-center`}>ABOUT<br/>ME</h2>
       <div className='relative h-[251px] mx-[11.2%]' onClick={handleTap}>
-    <Image src={images[index]} alt='タップするたびに、順番に様々な理容師佐藤京介の画像が表示されています' fill className='object-contain'/>
+    <Image src={images[index]} alt='タップするたびに、順番に様々な理容師佐藤京介の画像が表示されています' fill className='object-cover' />
     </div>
       <p>フェードやクロップを始めたきっかけは「男らしさを<br/>際立たせるため」。施術では肩の力を抜いて過ごせる<br/>よう<span>、</span>居心地を大切にしています。</p>
     </section>
