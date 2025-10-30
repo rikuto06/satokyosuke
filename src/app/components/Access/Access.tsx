bbc370d1b00ca1fef5918fa5d9e3e89e55447ace
rikuto06 <zhongweiluren0@gmail.com>
2025-10-30 16:17:45 +0900

    fix

--- src/app/components/Access/Access.tsx
+++ src/app/components/Access/Access.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 export default function Access() {
   return (
-    <section className='px-[5.3%] mt-[49px] md:mt-[100px] pb-[34px] md:pb-[100px] flex flex-col md:flex-row md:gap-[10%]'>
+    <section className='px-[5.3%] mt-[49px] md:mt-[100px] pb-[34px] md:pb-[100px] flex flex-col md:flex-row md:gap-[10%] md:justify-between'>
 
     <div className='contents md:block'>
       <h2 className={`${oswald.className} rotate-[0.03deg] text-[clamp(6.4rem,10vw,22.1rem)] text-transparent font-bold leading-[100%] [-webkit-text-stroke-width:1px] [-webkit-text-stroke-color:#F5F5F5]`} id='access'>ACCESS</h2>
